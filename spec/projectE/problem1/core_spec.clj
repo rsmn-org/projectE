39bd3526956c0e7cf8e60c1f7a2444de1a817b5a
daniel <daniel@rsmn.org>
2015-01-15 22:29:06 +0100

    add another much more faster solution to the problem

--- spec/projectE/problem1/core_spec.clj
+++ spec/projectE/problem1/core_spec.clj
@@ -3,12 +3,12 @@
             [projectE.problem1.core :refer :all]))
 
 (describe "list all the natural numbers below a border"
-           (it "tests that all numbers below 1 are shown"
-               (should= [0] (natural-numbers-below 1)))
-           (it "tests that all numbers below 2 are shown"
-               (should= [0 1] (natural-numbers-below 2)))
-           (it "tests that all numbers below 3 are shown"
-               (should= [0 1 2] (natural-numbers-below 3))))
+          (it "tests that all numbers below 1 are shown"
+              (should= [0] (natural-numbers-below 1)))
+          (it "tests that all numbers below 2 are shown"
+              (should= [0 1] (natural-numbers-below 2)))
+          (it "tests that all numbers below 3 are shown"
+              (should= [0 1 2] (natural-numbers-below 3))))
 
 (describe "list all the natural numbers below a border that are multiples of 3"
           (it "tests that all numbers below 1 are shown that are multiples of 3"
@@ -42,8 +42,32 @@
           (it "tests that the sum of these multiples"
               (should= 23 (problem1-example))))
 
-(describe "If we list all the natural numbers below 1000 that are multiples of 3 or 5"
+(describe "If we list all the natural numbers below 1000 that are multiples of 3 or 5 solution2"
           (it "tests that the sum of these multiples"
-              (should= 233168 (problem1-question))))
+              (should= 233168 (problem1-solution1))))
+
+(describe "If we list all the natural numbers below 1000 that are multiples of 3 or 5 with solution2"
+          (it "tests that the sum of these multiples"
+              (should= 233168 (problem1-solution2))))
+
+(comment
+  (describe "The Performance should be good"
+            (it "tests that the solution1 is returned in less than"
+                (should (time (problem1-solution1)))))
+
+  (describe "The Performance should be good"
+            (it "tests that the solution2 is returned in less than"
+                (should (time (problem1-solution2))))))
+
+(describe "Calc the largest multiple of x under n"
+          (it "test that largest multiple of 3 under 10"
+              (should= 9 (largest-multiple-of-x-under-n 3 10))))
+
+(describe "Calc the amount of multiple of x under n"
+          (it "test that the amount of multiple of 3 under 10"
+              (should= 4 (amount-of-numbers-div-by-x-under-n 3 10)))
+          (it "test that the amount of multiple of 5 under 10"
+              (should= 2 (amount-of-numbers-div-by-x-under-n 5 10))))
+
 
 (run-specs)
--- src/projectE/problem1/core.clj
+++ src/projectE/problem1/core.clj
@@ -14,12 +14,31 @@
 (defn sum-of [coll]
   (reduce + coll))
 
+(defn largest-multiple-of-x-under-n [x n]
+  (* x (quot n x)))
+
+(defn amount-of-numbers-div-by-x-under-n [x n]
+  (inc (quot (dec n) x)))
+
+(defn sum-of-number-div-by-x-under-n [x n]
+  (quot (* (largest-multiple-of-x-under-n x n)
+           (amount-of-numbers-div-by-x-under-n x (dec n))) 
+        2))
+
+(defn solve-fast [n]
+  (- (+ (sum-of-number-div-by-x-under-n 3 n)
+        (sum-of-number-div-by-x-under-n 5 n))
+     (sum-of-number-div-by-x-under-n 15 n)))
+
 (defn problem1-example []
   (sum-of (natural-numbers-below 10 (multiples-of 3 5))))
 
-(defn problem1-question []
+(defn problem1-solution1 []
   (sum-of (natural-numbers-below 1000 (multiples-of 3 5))))
+
+(defn problem1-solution2 []
+  (solve-fast 1000))
 
 (defn -main
   []
-  (println "The sum of all natural numbers below 1000 that are multiples of 3 or 5 is" (problem1-question)))
+  (println "The sum of all natural numbers below 1000 that are multiples of 3 or 5 is" (problem1-solution1)))
